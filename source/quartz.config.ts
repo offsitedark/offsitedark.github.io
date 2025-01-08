df646307ce77470dc44cafde4b334e7fb026f987
offsitedark <offsitedark@proton.me>
2025-01-08 17:58:42 -0600

    Update to title
    removed computer emoji

--- source/quartz.config.ts
+++ source/quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "💻🪶 Offsitedark",
+    pageTitle: "🪶 Offsitedark",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
